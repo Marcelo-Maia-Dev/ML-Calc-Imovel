f483557ed0b823aed72de69e3ba02eabcdade7b8
Marcelo Maia <marcelo.maia962@gmail.com>
2023-05-07 19:07:42 -0300

    MRP

--- Calc/app.py
+++ Calc/app.py
@@ -1,12 +1,16 @@
 # importar os pacotes 
 import flask 
+import pandas as pd 
 
-app = flask.Flask(__name__, template_folder="templates")
+app = flask.Flask(__name__, template_folder='templates')
 
 @app.router("/")
 def main():
-    return flask.render_template("index.html")
+    df = pd.read_csv("http://data.insideairbnb.com/brazil/rj/rio-de-janeiro/2019-07-15/visualisations/listings.csv")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+    return flask.render_template('index.html')
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
  
