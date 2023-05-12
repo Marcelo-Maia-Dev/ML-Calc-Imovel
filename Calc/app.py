49c4da58a263fca684c15183e8c73266ea7891b9
Marcelo_Maia <33229102+Marcelo-Maia-Dev@users.noreply.github.com>
2023-05-12 20:38:30 -0300

    Add files via upload

--- Calc/app.py
+++ Calc/app.py
@@ -1,23 +1,13 @@
-# importar os pacotes 
-import flask 
-import pandas as pd 
+# importando pacotes 
+import flask
+from flask import Flask
 
-app = flask.Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates')
 
-@app.router("/")
+@app.route('/')
 def main():
-    df = pd.read_csv("http://data.insideairbnb.com/brazil/rj/rio-de-janeiro/2019-07-15/visualisations/listings.csv")
+    return flask.render_template("home.html")
 
-    preco_medio = df['price'].mean()
-    num_entradas = df.shape[0]
-    lista_bairros = df['neighbourhood'].unique()
+if __name__ == "__main__":
+    app.run()
 
-    print(lista_bairros)
-   
-
-
-    return flask.render_template('index.html')
- 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
- 
